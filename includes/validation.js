8803e77a06d73c7d87bc9436b524586d96f06bdc
Roberto de Leon <robedeleong@gmail.com>
2021-11-04 17:38:44 -0600

    changed account route and updated nodemon version

--- includes/validation.js
+++ includes/validation.js
@@ -25,21 +25,11 @@ export const passwordValidation = (data) => {
 
 export const loginValidation = (data) => {
 	const schema = Joi.object({
-		username: Joi.string().min(6).max(25).required(),
+		username: Joi.string().min(3).max(25).required(),
 		password: Joi.string()
 			.min(6)
 			.required()
-			.pattern(new RegExp("^[a-zA-Z0-9]{3,30}$")),
 	});
 
 	return schema.validate(data);
 };
-
-// export const taskValidation = (data) => {
-// 	const schema = Joi.object({
-// 		title: Joi.string().min(3).required(),
-// 		teacher: Joi.string().min(3).required(),
-// 	});
-
-// 	return schema.validate(data);
-// };
